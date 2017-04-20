8df9c8b12b92a919bedb0797b4486d2f2129dde5
Simon Lamb <silamb@microsoft.com>
2017-04-20 22:01:13 +1000

    Initial commit

---
+++ hockeyapp.js
@@ -0,0 +1,74 @@
+var tl = require('vsts-task-lib');
+
+var appID = tl.getInput("appID", true);
+var apiToken = tl.getInput("apiToken", true);
+var installPackage = tl.getInput("installPackage", false);
+var buildNumber = tl.getVariable("Build.BuildNumber");
+
+console.log("Build Number: " + buildNumber);
+console.log("Install Package: " + installPackage);
+
+//Compress output directory
+/*
+var fs = require("fs");
+var zip = new require('node-zip')();
+zip.file(packageDirectory);
+var data = zip.generate({base64:false,compression:'DEFLATE'});
+fs.writeFileSync('upload.zip', data, 'binary');
+
+console.log("Zipped file: " + fs.file); //, Size: $($zipFile.length)"
+*/
+
+var request = require('request');
+
+var options = {
+  url: "https://rink.hockeyapp.net/api/2/apps/" + appID + "/app_versions",
+  headers: {
+    "X-HockeyAppToken": apiToken
+  }
+};
+
+var nextVersion = 1;
+
+request(options, function (error, response, body) {
+   if (!error && response.statusCode == 200) {
+	  var options = {
+		url: "https://rink.hockeyapp.net/api/2/apps/" + appID + "/app_versions/new",
+		headers: {
+		  "X-HockeyAppToken": apiToken
+		},
+		form:  { bundle_version: buildNumber }
+	  };
+
+	  request(options, function (error, response, body) {
+		if (!error) {
+			console.log("Response: " + body);
+			var response = JSON.parse(body);
+			
+			var fs = require("fs");
+      var glob = require("glob");
+      
+      glob(installPackage, function(err,files){
+        if (err) throw err;
+        var item = files[0];
+        
+        var req = request.put({ url: "https://rink.hockeyapp.net/api/2/apps/" + appID + "/app_versions/" + response.id, headers: {"X-HockeyAppToken": apiToken }}, 
+			    function (error, response, body) {
+				  console.log(body);
+        })
+        
+        console.log(item + " found");
+        var form = req.form();
+        form.append('ipa', fs.createReadStream(item));
+        form.append('status', 2);
+        form.append('notify', 1);        
+      });
+		}
+	  })  
+   }
+})
+
+
+
+
+
